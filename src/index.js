76c1ad379fd61d8657791d3d500f82a53ad1a9c3
Blue <lionofnorth814@gmail.com>
2025-08-17 04:30:54 -0700

    add api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log("this is index.js");
+fetch("openai.com/api/v1/engines")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
+console.log("this is end of index.js");
+console.log("This is the end of the file.");
+console.log("This is the start of the file.");
+console.log("This is a test log message.");
+console.log("This is another test log message.");
